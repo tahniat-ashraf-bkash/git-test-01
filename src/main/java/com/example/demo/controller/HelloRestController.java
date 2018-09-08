2c6be1a128add71b4f296f46b4537c4708c13609
Tahniat Ashraf Priyam <tahniat88@gmail.com>
2018-09-08 19:34:21 +0600

    First commit

---
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/abuse")
+	public String abuse() {
+		return "say a lot of bad words";
+	}
+}
